b6c693e5578f0bb85ec1f3a4780918a04333d14a
Xiao <xiao@scp-079.org>
2019-09-06 01:57:39 +0000

    - 增加 filter

--- plugins/functions/filters.py
+++ plugins/functions/filters.py
@@ -42,6 +42,17 @@ def is_exchange_channel(_, message: Message) -> bool:
     return False
 
 
+def is_from_user(_, message: Message) -> bool:
+    # Check if the message is sent from a user
+    try:
+        if message.from_user:
+            return True
+    except Exception as e:
+        logger.warning(f"Is from user error: {e}", exc_info=True)
+
+    return False
+
+
 def is_github_bot(_, message: Message) -> bool:
     # Check if the message is sent from the github bot
     try:
@@ -84,6 +95,11 @@ def is_test_group(_, message: Message) -> bool:
 exchange_channel = Filters.create(
     func=is_exchange_channel,
     name="Exchange Channel"
+)
+
+from_user = Filters.create(
+    func=is_from_user,
+    name="From User"
 )
 
 github_bot = Filters.create(
